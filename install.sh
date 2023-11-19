d16c6ebbe529caf19f86ef34965c05f960ffb589
Tom Willwerth <tom@tkwj.com>
2023-11-18 19:41:38 -0500

    install updates

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/backgrounds
+
 apt update
 apt upgrade -y
 
